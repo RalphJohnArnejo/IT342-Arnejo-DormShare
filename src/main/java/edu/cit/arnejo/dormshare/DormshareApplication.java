8ae7ebdf7b146bebe7ab5dade4f60dd4778fb742
LAB13_USER ON WS42 <L13Y12W42@LabHCI.net>
2026-03-07 11:18:31 +0800

    chore: initial Spring Boot setup targeting Java 17

---
+++ src/main/java/edu/cit/arnejo/dormshare/DormshareApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.arnejo.dormshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormshareApplication.class, args);
+	}
+
+}
